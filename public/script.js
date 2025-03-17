036b6ec6d08fd56cd10b63ff3cfe0f5673c98f22
gabi <gabialvarenga4@gmail.com>
2025-03-16 21:00:35 -0300

    Finalizando

--- public/script.js
+++ public/script.js
@@ -1,8 +1,32 @@
 $(function () {
-    $("#mobile-btn").on("click", function () { 
-        $("#mobile-menu").toggleClass("active");
+    $('#mobile-btn').on("click", function () { 
+        $('#mobile-menu').toggleClass('active');
         $(this).find('i').toggleClass('fa-x'); 
     });
 
 
+    const sections = $('section');
+    const navItens = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+
+        if(scrollPosition <=0 ){
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0,0,0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBotton = sectionTop = section.outerHeight();
+
+            if(scrollPosition)
+        })
+    })
 });
